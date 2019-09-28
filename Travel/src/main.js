c880710076fbd541c64c777c85ec4998cd2a92b8
purplezz <18672780120@163.com>
2019-09-28 15:21:06 +0800

    add header

--- Travel/src/main.js
+++ Travel/src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
---
+++ Travel/src/pages/home/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe658;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe63c;</span>
+      输入游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe659;</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  line-height: 43px;
+  background: #00bcd4;
+  color: #fff;
+}
+.header-left {
+  width: 32px;
+  float: left;
+}
+.header-input {
+  flex: 1;
+  height: 32px;
+  line-height: 32px;
+  margin-top: 5px;
+  margin-left: 5px;
+  padding-left: 5px;
+  background: #fff;
+  border-radius: 10px;
+  color: #ccc;
+}
+.header-right {
+  width: 50px;
+  float: right;
+  text-align: center;
+}
+.back-icon {
+  text-align: center;
+  font-size: 20px;
+}
+</style>
